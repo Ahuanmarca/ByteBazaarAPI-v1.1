d7d0f775d1dff72eb368930e92d74fab49e37ee7
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-04 12:18:30 +0100

    BUG FIX: Remove 'unique' restriction from 'genresId' foreign key field on GameTitle model
    - The 'unique' restriction caused a bug where it was not possible to insert repeated foreign keys in different documents.
    - Even worse, it was not enough to remove the restriction because it creates some 'index' on MongoDB that then must be removed in another way.
    - Also added an explicit warning (a comment) in the GameTitle model, because this is quite obscure for me.

--- src/api/gameTitles/gameTitles.model.js
+++ src/api/gameTitles/gameTitles.model.js
@@ -17,7 +17,14 @@ const gameTitlesSchema = new Schema({
   genresId: [{
     type: ObjectId,
     ref: 'Genre',
-    unique: true, // this doesn't seem to create a restriction
+    /**
+     * ! CAREFUL
+     * ! Please don't use 'unique: true' restriction here
+     * It casues error when inserting repeated ObjectIds (as foreign keys)
+     * in different documents, but the real problem is that you can't just
+     * delete the restriction to fix the problem because it creates an 'index'
+     * on MongoDb that you need to delete manually (I don't know how).
+     */
   }],
 });
 
