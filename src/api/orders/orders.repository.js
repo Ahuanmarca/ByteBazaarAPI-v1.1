62bd11f4379af1a33f945e1fd61fbf9c48b3a328
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-05 10:01:28 +0100

    Updated "Get Orders By User Id" so it populates the foreign keys, so it's usefull for the frontend without additional queries.

--- src/api/orders/orders.repository.js
+++ src/api/orders/orders.repository.js
@@ -2,14 +2,28 @@ import OrdersModel from './orders.model.js';
 
 async function getOrdersByUserId({ userId }) {
   const orders = await OrdersModel
-    .find({ user_id: userId })
+    .find({ user: userId })
     .sort({ date: -1 })
+    .populate({
+      path: 'products.product',
+      populate: [
+        {
+          path: 'gameTitle',
+          populate: {
+            path: 'genres',
+          },
+        },
+        {
+          path: 'platform',
+        },
+      ],
+    })
     .lean();
   return orders;
 }
 
 async function log({ userId, total }) {
-  const order = await ordersModel.create({ user_id: userId, total });
+  const order = await OrdersModel.create({ user: userId, total });
   const { _id } = order;
   return _id;
 }
