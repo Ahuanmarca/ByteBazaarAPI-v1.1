ca6e3bde3bbd66366fa0e53d9d0fad3ce75912f6
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-06 08:47:48 +0100

    Uncommented isLogged on index.js, tested auth. Working ok.
    - Updated Alice, Bob and Charlie passowrds to be equal to their names (lowercased).

--- src/api/products/products.controller.js
+++ src/api/products/products.controller.js
@@ -1,19 +1,5 @@
 import * as productsService from './products.service.js';
 
-// DEBUG DATA
-const debugUser = {
-  _id: '65c092e67aaecf4937ffad30',
-  firstName: 'Bob',
-  lastName: 'Marley',
-  isAdmin: false,
-  email: 'bob@bob.bo',
-  password: '$2b$05$wR9P9tH0fOQjnpCHmYN5jepdnr1bKhxnk.gN87tA.LjSyh9gONGo.',
-  validated: true,
-  credit: 500,
-  points: 0,
-  __v: 0,
-};
-
 async function getAll(req, res) {
   const { skip, limit } = req.query;
   const products = await productsService.getAll({ skip, limit });
@@ -41,7 +27,7 @@ async function getRelated(req, res) {
 
 async function buy(req, res) {
   const { body } = req;
-  const user = req.body.user || debugUser; // Use DEBUG User if not logged
+  const { user } = req;
   const result = await productsService.buy({ orderData: body, user });
   if (result.error) {
     res.status(400);
