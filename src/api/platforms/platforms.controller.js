9ac24fbef3b318fb44d64045b15cb67340f5ce43
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-06 20:56:27 +0100

    Modify platforms.controller and products.controller to return the plain objects
    Details:
    
    Some controllers were returning their responses like this:
    
      res.json({ thing });
    
    Others like this:
    
      res.json(thing);
    
    We will go with the second option for consistency and to have less nesting on the responses.

--- src/api/platforms/platforms.controller.js
+++ src/api/platforms/platforms.controller.js
@@ -3,7 +3,7 @@ import * as platformsService from './platforms.service.js';
 async function getById(req, res) {
   const { id } = req.params;
   const platform = await platformsService.getById({ id });
-  res.json({ platform });
+  res.json(platform);
 }
 
 export {
--- src/api/products/products.controller.js
+++ src/api/products/products.controller.js
@@ -3,26 +3,26 @@ import * as productsService from './products.service.js';
 async function getAll(req, res) {
   const { skip, limit } = req.query;
   const products = await productsService.getAll({ skip, limit });
-  res.json({ products });
+  res.json(products);
 }
 
 async function getById(req, res) {
   const { id } = req.params;
   const product = await productsService.getById({ id });
-  res.json({ product });
+  res.json(product);
 }
 
 async function getRecommended(req, res) {
   const userId = req.user?._id || '65c092e67aaecf4937ffad2f'; // DEBUG User with orders
   // const userId = req.user?._id || '65c092e67aaecf4937ffad31'; // DEBUG User without orders
   const products = await productsService.getRecommended({ userId });
-  res.json({ products });
+  res.json(products);
 }
 
 async function getRelated(req, res) {
   const { id } = req.params;
   const products = await productsService.getRelated({ id });
-  res.json({ products });
+  res.json(products);
 }
 
 async function buy(req, res) {
