4d36c44ebede64e978b39e7db5314fa4a1570f45
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-15 18:36:37 +0100

    Updated platforms routes

--- src/api/platforms/platforms.repository.js
+++ src/api/platforms/platforms.repository.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-console */
 import PlatformModel from './platforms.model.js';
 
 async function getById({ id }) {
-  const platform = await PlatformModel.findById(id).lean();
-  return platform;
+  try {
+    const platform = await PlatformModel.findById(id).lean();
+    return platform;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
 }
 
 export {
